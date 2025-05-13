44f9ef227798629ed77dcc366c43bd352869856d
Khushal Singh Rathore <khushalrathore1515@gmail.com>
2025-05-13 09:03:59 +0530

    pattern if case

--- lib/data.dart
+++ lib/data.dart
@@ -1,14 +1,69 @@
 import 'dart:convert';
 
+// class Document {
+//   final Map<String, Object?> _json;
+//   Document()
+//     : _json = jsonDecode(
+//         documentJson,
+//       ); // The : here is an initlizer list. This is used to call the function before the constructor body called. Basically it runs before the constructor body.
+
+//   (String, {DateTime modified}) get metadata {
+//     const title = 'My Document';
+//     final now = DateTime.now();
+
+//     return (title, modified: now);
+//   }
+// }
+
 class Document {
   final Map<String, Object?> _json;
   Document() : _json = jsonDecode(documentJson);
 
   (String, {DateTime modified}) get metadata {
-    const title = 'My Document';
-    final now = DateTime.now();
+    // if (_json.containsKey('metadata')) {
+    //   // Modify from here...
+    //   final metadataJson = _json['metadata'];
+    //   if (metadataJson is Map) {
+    //     final title = metadataJson['title'] as String;
+    //     final localModified = DateTime.parse(
+    //       metadataJson['modified'] as String,
+    //     );
+    //     return (title, modified: localModified);
+    //   }
+    // }
+    // throw const FormatException('Unexpected JSON'); // to here.
 
-    return (title, modified: now);
+    if (_json // Modify from here...
+        case {
+          'metadata': {'title': String title, 'modified': String localModified},
+        }) {
+      return (title, modified: DateTime.parse(localModified));
+    } else {
+      throw const FormatException('Unexpected JSON');
+    }
+  }
+
+  List<Block> getBlocks() {
+    // Add from here...
+    if (_json case {'blocks': List blocksJson}) {
+      return [for (final blockJson in blocksJson) Block.fromJson(blockJson)];
+    } else {
+      throw const FormatException('Unexpected JSON format');
+    }
+  }
+}
+
+class Block {
+  final String type;
+  final String text;
+  Block(this.type, this.text);
+
+  factory Block.fromJson(Map<String, dynamic> json) {
+    if (json case {'type': final type, 'text': final text}) {
+      return Block(type, text);
+    } else {
+      throw const FormatException('Unexpected JSON format');
+    }
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -22,17 +22,49 @@ class DocumentScreen extends StatelessWidget {
   final Document document;
 
   const DocumentScreen({required this.document, super.key});
+  @override
+  Widget build(BuildContext context) {
+    final (title, modified: modified) = document.metadata;
+    final blocks = document.getBlocks();
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Column(
+        children: [
+          Text('Last modified: $modified'), // Modify from here
+          Expanded(
+            child: ListView.builder(
+              itemCount: blocks.length,
+              itemBuilder: (context, index) {
+                return BlockWidget(block: blocks[index]);
+              },
+            ),
+          ), // to here.
+        ],
+      ),
+    );
+  }
+}
+
+class BlockWidget extends StatelessWidget {
+  final Block block;
+
+  const BlockWidget({required this.block, super.key});
 
   @override
   Widget build(BuildContext context) {
-    final metaDataRecord = document.metadata;
-    return Scaffold(
-      appBar: AppBar(title: Text(metaDataRecord.$1)),
-      body: Column(
-        children: [
-          Center(child: Text('Last modified ${metaDataRecord.modified}')),
-        ],
-      ),
+    TextStyle? textStyle;
+    switch (block.type) {
+      case 'h1':
+        textStyle = Theme.of(context).textTheme.displayMedium;
+      case 'p' || 'checkbox':
+        textStyle = Theme.of(context).textTheme.bodyMedium;
+      case _:
+        textStyle = Theme.of(context).textTheme.bodySmall;
+    }
+
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: Text(block.text, style: textStyle),
     );
   }
 }
